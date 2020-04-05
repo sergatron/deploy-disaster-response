eacd608c9dd2f1b3bd2aef72cd4e5dd7e3d11fcf
Sergey M <smouzykin@gmail.com>
2020-04-04 20:17:01 -0400

    Add NLTK downloads for stop-words

--- runapp.py
+++ runapp.py
@@ -1,4 +1,7 @@
 from webapp import app
+import nltk
+
+nltk.download(['stopwords', 'punkt', 'wordnet', 'averaged_perceptron_tagger', 'maxent_ne_chunker', 'words',])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
